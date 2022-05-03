caf288eecf01766da6ccef9c87c5cdf18b1f21df
MiIlenium <103943021+MiIlenium@users.noreply.github.com>
2022-05-03 17:39:16 +0300

    Create Builder.java

---
+++ HW-11-04/HumanBeing/Builder.java
@@ -0,0 +1,7 @@
+package HumanBeing;
+
+public class Builder extends Human {
+    public Builder (String name, int age, String gender, String language){
+        super(name, age, gender, language);
+    }
+}
